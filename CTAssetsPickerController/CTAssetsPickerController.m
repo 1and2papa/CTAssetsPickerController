263f2403a24a30764ee6d2c152ab5ee13ed7c349
Clement T <chiunam@gmail.com>
2014-02-14 11:29:58 +0800

    Refactor draw rect

--- CTAssetsPickerController/CTAssetsPickerController.m
+++ CTAssetsPickerController/CTAssetsPickerController.m
@@ -1083,9 +1083,9 @@ static UIColor *disabledColor;
 {
     if (self = [super initWithFrame:frame])
     {
-        self.opaque                     = YES;
-        self.isAccessibilityElement     = YES;
-        self.accessibilityTraits        = UIAccessibilityTraitImage;
+        self.opaque                 = YES;
+        self.isAccessibilityElement = YES;
+        self.accessibilityTraits    = UIAccessibilityTraitImage;
     }
     
     return self;
@@ -1110,67 +1110,80 @@ static UIColor *disabledColor;
 }
 
 
-// Draw everything to improve scrolling responsiveness
+#pragma mark - Draw Rect
+
 - (void)drawRect:(CGRect)rect
 {
-    // Image
-    [self.image drawInRect:CGRectMake(0, 0, kThumbnailLength, kThumbnailLength)];
-    
-    // Video title
-    if ([self.type isEqual:ALAssetTypeVideo])
-    {
-        // Create a gradient from transparent to black
-        CGFloat colors [] = {
-            0.0, 0.0, 0.0, 0.0,
-            0.0, 0.0, 0.0, 0.8,
-            0.0, 0.0, 0.0, 1.0
-        };
-        
-        CGFloat locations [] = {0.0, 0.75, 1.0};
-        
-        CGColorSpaceRef baseSpace   = CGColorSpaceCreateDeviceRGB();
-        CGGradientRef gradient      = CGGradientCreateWithColorComponents(baseSpace, colors, locations, 2);
-        
-        CGContextRef context    = UIGraphicsGetCurrentContext();
-        
-        CGFloat height          = rect.size.height;
-        CGPoint startPoint      = CGPointMake(CGRectGetMidX(rect), height - titleHeight);
-        CGPoint endPoint        = CGPointMake(CGRectGetMidX(rect), CGRectGetMaxY(rect));
-        
-        CGContextDrawLinearGradient(context, gradient, startPoint, endPoint, kCGGradientDrawsBeforeStartLocation);
+    [self drawThumbnailInRect:rect];
 
-        CGColorSpaceRelease(baseSpace);
-        CGGradientRelease(gradient);
-        
-        CGSize titleSize        = [self.title sizeWithAttributes:@{NSFontAttributeName : titleFont}];
-        CGRect titleRect        = CGRectMake(rect.size.width - titleSize.width - 2, startPoint.y + (titleHeight - 12) / 2, titleSize.width, titleHeight);
-        
-        NSMutableParagraphStyle *titleStyle = [[NSMutableParagraphStyle alloc] init];
-        titleStyle.lineBreakMode = NSLineBreakByTruncatingTail;
-        
-        [self.title drawInRect:titleRect
-                withAttributes:@{NSFontAttributeName : titleFont,
-                                 NSForegroundColorAttributeName : titleColor,
-                                 NSParagraphStyleAttributeName : titleStyle}];
-        
-        [videoIcon drawAtPoint:CGPointMake(2, startPoint.y + (titleHeight - videoIcon.size.height) / 2)];
-    }
+    if ([self.type isEqual:ALAssetTypeVideo])
+        [self drawVideoMetaInRect:rect];
     
     if (self.disabled)
-    {
-        CGContextRef context    = UIGraphicsGetCurrentContext();
-		CGContextSetFillColorWithColor(context, disabledColor.CGColor);
-		CGContextFillRect(context, rect);
-    }
+        [self drawDisabledViewInRect:rect];
     
     else if (self.selected)
-    {
-        CGContextRef context    = UIGraphicsGetCurrentContext();
-		CGContextSetFillColorWithColor(context, selectedColor.CGColor);
-		CGContextFillRect(context, rect);
-        
-        [checkedIcon drawAtPoint:CGPointMake(CGRectGetMaxX(rect) - checkedIcon.size.width, CGRectGetMinY(rect))];
-    }
+        [self drawSelectedViewInRect:rect];
+}
+
+- (void)drawThumbnailInRect:(CGRect)rect
+{
+    [self.image drawInRect:CGRectMake(0, 0, kThumbnailLength, kThumbnailLength)];
+}
+
+- (void)drawVideoMetaInRect:(CGRect)rect
+{
+    // Create a gradient from transparent to black
+    CGFloat colors [] = {
+        0.0, 0.0, 0.0, 0.0,
+        0.0, 0.0, 0.0, 0.8,
+        0.0, 0.0, 0.0, 1.0
+    };
+    
+    CGFloat locations [] = {0.0, 0.75, 1.0};
+    
+    CGColorSpaceRef baseSpace   = CGColorSpaceCreateDeviceRGB();
+    CGGradientRef gradient      = CGGradientCreateWithColorComponents(baseSpace, colors, locations, 2);
+    
+    CGContextRef context    = UIGraphicsGetCurrentContext();
+    
+    CGFloat height          = rect.size.height;
+    CGPoint startPoint      = CGPointMake(CGRectGetMidX(rect), height - titleHeight);
+    CGPoint endPoint        = CGPointMake(CGRectGetMidX(rect), CGRectGetMaxY(rect));
+    
+    CGContextDrawLinearGradient(context, gradient, startPoint, endPoint, kCGGradientDrawsBeforeStartLocation);
+    
+    CGColorSpaceRelease(baseSpace);
+    CGGradientRelease(gradient);
+    
+    CGSize titleSize        = [self.title sizeWithAttributes:@{NSFontAttributeName : titleFont}];
+    CGRect titleRect        = CGRectMake(rect.size.width - titleSize.width - 2, startPoint.y + (titleHeight - 12) / 2, titleSize.width, titleHeight);
+    
+    NSMutableParagraphStyle *titleStyle = [[NSMutableParagraphStyle alloc] init];
+    titleStyle.lineBreakMode = NSLineBreakByTruncatingTail;
+    
+    [self.title drawInRect:titleRect
+            withAttributes:@{NSFontAttributeName : titleFont,
+                             NSForegroundColorAttributeName : titleColor,
+                             NSParagraphStyleAttributeName : titleStyle}];
+    
+    [videoIcon drawAtPoint:CGPointMake(2, startPoint.y + (titleHeight - videoIcon.size.height) / 2)];
+}
+
+- (void)drawDisabledViewInRect:(CGRect)rect
+{
+    CGContextRef context = UIGraphicsGetCurrentContext();
+    CGContextSetFillColorWithColor(context, disabledColor.CGColor);
+    CGContextFillRect(context, rect);
+}
+
+- (void)drawSelectedViewInRect:(CGRect)rect
+{
+    CGContextRef context    = UIGraphicsGetCurrentContext();
+    CGContextSetFillColorWithColor(context, selectedColor.CGColor);
+    CGContextFillRect(context, rect);
+    
+    [checkedIcon drawAtPoint:CGPointMake(CGRectGetMaxX(rect) - checkedIcon.size.width, CGRectGetMinY(rect))];
 }
 
 
