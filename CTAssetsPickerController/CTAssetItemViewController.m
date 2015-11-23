eea4e8252c3be9a8b1bc014a9b33659e350ac722
Clement T <chiunam@gmail.com>
2015-11-23 11:32:15 +0800

    Update copyright date

--- CTAssetsPickerController/CTAssetItemViewController.m
+++ CTAssetsPickerController/CTAssetItemViewController.m
@@ -2,7 +2,7 @@
  
  MIT License (MIT)
  
- Copyright (c) 2013 Clement CN Tsang
+ Copyright (c) 2015 Clement CN Tsang
  
  Permission is hereby granted, free of charge, to any person obtaining a copy
  of this software and associated documentation files (the "Software"), to deal
--- CTAssetsPickerDemo/Examples/CTApperanceViewController.m
+++ CTAssetsPickerDemo/Examples/CTApperanceViewController.m
@@ -2,7 +2,7 @@
  
  MIT License (MIT)
  
- Copyright (c) 2013 Clement CN Tsang
+ Copyright (c) 2015 Clement CN Tsang
  
  Permission is hereby granted, free of charge, to any person obtaining a copy
  of this software and associated documentation files (the "Software"), to deal
@@ -121,6 +121,7 @@
     assetsGridSelectedView.font = [self.font fontWithSize:18.0];
     assetsGridSelectedView.textColor = self.color2;
     assetsGridSelectedView.borderWidth = 1.0;
+    assetsGridSelectedView.isLabelCircular = YES;
         
     // check mark
     [CTAssetCheckmark appearance].tintColor = self.color1;
